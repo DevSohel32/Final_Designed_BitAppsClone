1024cc642189176271b8b6a3d4a757412beb6ede
sujauddoula352 <sujauddoulasohel352@gmail.com>
2025-09-28 02:21:25 +0600

    feat: Implement basic navigation and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
   /** @type {import('tailwindcss').Config} */
  export default {
+  content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
     },
-    plugins: [],
+     plugins: [require("daisyui")],
   }
